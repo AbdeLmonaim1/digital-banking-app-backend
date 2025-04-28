71cea80d59b9a0a8727bd3e5e8d8d7933c9465fa
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-04-28 15:12:39 +0100

    DigitalBankingAppBackendApplication

---
+++ src/main/java/ma/enset/digitat/banking/DigitalBankingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.digitat.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingAppBackendApplication.class, args);
+    }
+
+}
